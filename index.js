ab27f7484e6f50b80a97c8001626b6562fb4825f
Edgar <eddyade98@gmail.com>
2023-10-27 01:00:41 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Add your code here
+  function submitData (userName, userEmail) {
+
+    const formData = {
+        userName: "Dan",
+        userEmail: "Poodle@gmail.com",
+      };
+
+      const userData = {
+        method: 'POST', 
+        headers: {
+          'Content-Type': 'application/json', 
+          Accept: 'application/json',
+        },
+        body: JSON.stringify(formData),
+    };
+  }
+    
+fetch("http://localhost:3000/users", submitData)
+
+.then(function (response) {
+    return response.json();
+  })
+  .then(function (object) {
+    console.log(object);
+  })
+  .catch(function (error) {
+    alert("Not Good!");
+    console.log(error.message);
+  });
+
+  
+
